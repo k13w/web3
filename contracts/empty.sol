5f9c0b8c3af23c874268f17564e0f2c3b103001c
Gilmar Silva <gilmarheavensilva@hotmail.com>
2024-12-28 13:00:27 -0300

    chore: att sol

--- contracts/empty.sol
+++ contracts/empty.sol
@@ -6,7 +6,7 @@ pragma solidity >=0.7.0 <0.9.0;
  * @title Ballot
  * @dev Implements voting process along with vote delegation
  */
-contract Ballote {
+contract Ballotew {
     // This declare a new complex type which will
     // be used for variables later.
     // It will represent a single voter.
