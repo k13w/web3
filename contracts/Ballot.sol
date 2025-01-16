ac98213e1958071a2f215bee88b9b477d915ae06
Gilmar Silva <gilmarheavensilva@hotmail.com>
2025-01-16 18:33:47 -0300

    feat: britney nao vai me lascar hj

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -9,7 +9,7 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Ballotetete {
     // This declares a new complex type which will
     // be used for variables later.
-    // It will represent a single voter.
+    // It will represenst a single voter.
     struct Voter {
         uint weight; // weight is accumulated by delegation
         bool voted;  // if true, that person already voted
