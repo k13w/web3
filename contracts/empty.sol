7556caef59e34a4e35a2dae35b753d132418be7a
Gilmar Silva <gilmarheavensilva@hotmail.com>
2025-01-20 18:49:09 -0300

    fix: readme

--- contracts/empty.sol
+++ contracts/empty.sol
@@ -14,7 +14,7 @@ contract Ballotew {
         uint weight; // weight is accumulated by delegation
         bool voted;  // if true, that person already voted
         address delegate; // person delegated to
-        uint vote;   // index of the voted proposal
+        uint vote;   // aindex of the voted proposal
     }
 
     // This is a type for a single proposal.
