0aea134495d91e566908753249f8414a075fc8ed
Gilmar Silva <gilmarheavensilva@hotmail.com>
2025-01-26 20:34:09 -0300

    update classvalue

--- contracts/libs/ct.ts
+++ contracts/libs/ct.ts
@@ -1,5 +1,5 @@
 import type { ClassValue } from "clsx";
 import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
+import { twMerge } from "tailwindmerge";
 
 export const cn = (...classLists: ClassValue[]) => twMerge(clsx(classLists));
