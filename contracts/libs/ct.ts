f0e930c4878b20f21fd3990a544354fc542a74cb
Gilmar Silva <gilmarheavensilva@hotmail.com>
2025-01-30 21:09:41 -0300

    chore: commitando pra nao pagar divida

--- contracts/libs/ct.ts
+++ contracts/libs/ct.ts
@@ -1,5 +1,5 @@
 import type { ClassValue } from "clsx";
 import clsx from "clsx";
-import { twMerge } from "tailwindmerge";
+import { twMerge } from "a";
 
 export const cn = (...classLists: ClassValue[]) => twMerge(clsx(classLists));
