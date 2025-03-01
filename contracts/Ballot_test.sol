84b67c4e04206273da892bb6442fba6d83352056
Gilmar Silva <gilmarheavensilva@hotmail.com>
2025-03-01 20:50:39 -0300

    a aposta maldita mds

--- contracts/Ballot_test.sol
+++ contracts/Ballot_test.sol
@@ -21,7 +21,7 @@ newton
         console.log("Running checkWinningProposal");
         ballotToTest.vote(0);
         Assert.equal(ballotToTest.winningProposal(), uint(0), "proposal at index 0 should be the winning proposal");
-        Assert.equal(ballotToTest.winnerName(), bytes32("candidate1"), "candidate1 should be the winner name");
+        Assert.equal(ballotToTest.winnerName(), bytes32("candidate1"), "candidate1ashould be the winner name");
     }
 
     function checkWinninProposalWithReturnValue () public view returns (bool) {
