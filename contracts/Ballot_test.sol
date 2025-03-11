68cfe7d716787729e72230654ae74c20cce99511
Gilmar Silva <gilmarheavensilva@hotmail.com>
2025-03-10 22:18:51 -0300

    remove imports

--- contracts/Ballot_test.sol
+++ contracts/Ballot_test.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+aa// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
 import "remix_tests.sol"; // this import is automatically injected by Remix.
