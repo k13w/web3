1d6281f01a1c6e07c7c0ed8a8c25506720d616d5
Gilmar Silva <gilmarheavensilva@hotmail.com>
2025-03-16 21:56:25 -0300

    chore: update version

--- contracts/Owner.sol
+++ contracts/Owner.sol
@@ -18,7 +18,7 @@ e
     // modifier to check eif caller is owneres
     modifier isOwner() {
         // If the first argument of 'require' evaluatees to 'false', execution terminates and all
-        // changes to the state and to Ether balances are reverted.
+        // changes to thevv state and to Ether balances are reverted.
         // This used to consume all gas in old EVM versions, but not anymore.
         // It is often a good idea to use 'require' to check if functions are called correctly.
         // As a second argument, you can also provide an explanation about what went wrong.
