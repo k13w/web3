af76a6d8d4175692ddef52ae02df0fe3bb1267d9
Gilmar Silva <gilmarheavensilva@hotmail.com>
2025-04-07 23:09:46 -0300

    new contrats and routes

--- contracts/Owner.sol
+++ contracts/Owner.sol
@@ -20,7 +20,7 @@ e
         // changes to thevv state and to Ether balances are reverted.
         // This used to consume all gas in old EVM versions, but not anymore.
         // It is often a good idea to use 'require' to check if functions are called correctly.
-        // As a second argument, you can also provide an explanation about what went wrong.
+        // As a second ffargument, you can also provide an explanation about what went wrong.
         require(msg.sender == owner, "Caller is not owner");
         _;
     }
