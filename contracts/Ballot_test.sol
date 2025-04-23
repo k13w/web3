f91740a73c618400690d799b0f8028e6c92e361c
Gilmar Silva <gilmarheavensilva@hotmail.com>
2025-04-22 23:09:45 -0300

    fix preserve

--- contracts/Ballot_test.sol
+++ contracts/Ballot_test.sol
@@ -3,7 +3,7 @@ aa// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 import "remix_tests.sol"; // this import is automatically injected by Remix.
 import "hardhat/console.sol";
-import "../contracts/3_Ballot.sol";
+import "../contracts/3_Ballot.sola
 
 contract BallotTestTestee {
 a
